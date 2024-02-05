5530ab8c9867b9b81adf56af749eee9d5bf2b7c4
ItsAngelN <iAmAngelN@iCloud.Com>
2024-02-04 22:14:05 -0600

    i added in the absolute formulas and corrisponding print functions

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,13 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    in_num  = int(input("Enter a number: "))
+    abso_num = abs(in_num - 21)
+    abso_dub = abs((in_num - 21) * 2)
+    if in_num > 21 :
+        print("Result:",abso_dub)
+    elif in_num < 21 :
+        print("Result:", abso_num)
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
